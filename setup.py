bc27e81c0832097a395e1ae00fafa0ddacc28751
Igor Ostrovsky <igor@augmentcode.com>
2022-12-23 06:39:40 +0000

    Release v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lm_plot",
-    version="0.1.0",
+    version="0.1.1",
     author="Igor Ostrovsky",
     author_email="igoros@gmail.com",
     description="A tool for visualizing Language Model Evaluation Harness results",
